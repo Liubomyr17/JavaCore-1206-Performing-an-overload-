41a2535eacface9056d3cdbc114e8bb5747ab7db
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-03-02 21:22:12 +0200

    Add files via upload

---
+++ JavaCore 1206 Performing an overload!/src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+/*
+
+1206 overload
+
+Method overload
+ 
+Overloading!
+Write two methods: print (int) and print (String).
+
+Requirements:
+1. The program should not display text on the screen.
+2. The Solution class must contain three methods.
+3. The Solution class must contain the static main () method.
+4. The Solution class must contain a static print () method with an int parameter.
+5. The Solution class must contain a static print () method with a parameter of type String.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int s) {
+
+    }
+
+    public static void print(String s) {
+
+    }
+}
